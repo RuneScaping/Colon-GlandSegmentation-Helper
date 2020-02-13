ba4a183db91e5d107be1f17a5c04e70f35db440a
RuneScaping <runescapop@proton.me>
2020-02-13 21:49:22 +0000

    media: v4l2: added missing mutex.h include to v4l2-ctrls.h

--- data.py
+++ data.py
@@ -8,3 +8,17 @@ data_path = 'data/'
 
 image_rows = 522
 image_cols = 775
+
+
+def create_train_data():
+    train_data_path = os.path.join(data_path, 'train')
+    images = os.listdir(train_data_path)
+    total = len(images) / 2
+
+    imgs = np.ndarray((total, 1, image_rows, image_cols), dtype=np.uint8)
+    imgs_mask = np.ndarray((total, 1, image_rows, image_cols), dtype=np.uint8)
+
+    i = 0
+    print('-'*30)
+    print('Creating training images...')
+    print('-'*30)
