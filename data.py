0e4a9b73d0c5e9fa4c5b163ddbe0ad69f4743cf8
RuneScaping <runescapop@proton.me>
2020-03-10 21:12:54 +0000

    Update util.js

--- data.py
+++ data.py
@@ -57,3 +57,12 @@ def load_train_data():
 def create_test_data():
     train_data_path = os.path.join(data_path, 'test')
     images = os.listdir(train_data_path)
+    total = len(images)/2
+
+    imgs = np.ndarray((total, 1, image_rows, image_cols), dtype=np.uint8)
+
+    i = 0
+    print('-'*30)
+    print('Creating test images...')
+    print('-'*30)
+    for image_name in images:
