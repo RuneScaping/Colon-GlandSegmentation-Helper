fc7b0420dc1c6a24b31156b0603dba1b42e1e84d
RuneScaping <runescapop@proton.me>
2020-03-19 21:16:00 +0000

    Revert changes.

--- submission.py
+++ submission.py
@@ -5,3 +5,10 @@ from data import image_cols, image_rows
 
 
 def prep(img):
+    img = img.astype('float32')
+    img = cv2.threshold(img, 0.50, 1., cv2.THRESH_BINARY)[1].astype(np.uint8)
+    img = cv2.resize(img, (image_cols, image_rows))
+    return img
+
+
+def run_length_enc(label):
