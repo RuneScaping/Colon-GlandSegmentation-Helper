1cd4867c6f699db0ad2b2888b4006da095f01734
RuneScaping <runescapop@proton.me>
2020-06-25 15:10:04 +0000

    test json

--- train.py
+++ train.py
@@ -2,4 +2,5 @@ import cv2
 import numpy as np
 from keras.models import Model
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers import Input, merge, SpatialDrop
+from keras.layers import Input, merge, SpatialDropout2D
+from k
