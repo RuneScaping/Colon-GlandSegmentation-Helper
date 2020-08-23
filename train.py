59fa9abb41fd72599f7c9b781b4bedf469a9c6fe
RuneScaping <runescapop@proton.me>
2020-08-23 11:58:18 +0000

    fixed variables

--- train.py
+++ train.py
@@ -3,4 +3,4 @@ import numpy as np
 from keras.models import Model
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers import Input, merge, SpatialDropout2D
-from keras.layers import Convolution2D, Ave
+from keras.layers import Convolution2D, AveragePooling2D, 
