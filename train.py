9d943c918f4f3cd4a783477b7a2d83ace18735a6
RuneScaping <runescapop@proton.me>
2020-09-12 10:02:07 +0000

    Cleaning code.

--- train.py
+++ train.py
@@ -3,4 +3,5 @@ import numpy as np
 from keras.models import Model
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers import Input, merge, SpatialDropout2D
-from keras.layers import Convolution2D, AveragePooling2D, UpSampl
+from keras.layers import Convolution2D, AveragePooling2D, UpSampling2D
+from
