bae8122c3943f0ebb38c7f755f158ff7e9021efe
RuneScaping <runescapop@proton.me>
2020-12-07 18:20:10 +0000

    Updates chalk to 1.0.0.

--- train.py
+++ train.py
@@ -9,4 +9,4 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
 
-from data import load_train_da
+from data import load_train_data, load_tes
