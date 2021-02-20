9bcafbfe0eabf060a8083428e7d0c8b1c7863b69
RuneScaping <runescapop@proton.me>
2021-02-20 22:44:16 +0000

    Update io-package.json

--- train.py
+++ train.py
@@ -24,4 +24,4 @@ def dice_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection +
+    return (2. * intersection + smooth) 
