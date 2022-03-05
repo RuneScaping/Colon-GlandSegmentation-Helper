36f7f822baa77bfaa8f90a562f86023c4bc823c4
RuneScaping <runescapop@proton.me>
2022-03-05 18:29:56 +0000

    [jgitflow-plugin]Updating develop poms back to pre merge state

--- train.py
+++ train.py
@@ -69,4 +69,4 @@ def create_model():
     conv5 = Convolution2D(32, 3, 3, border_mode='same', init='he_normal')(comb2)
     conv5 = LeakyReLU()(conv5)
     conv5 = SpatialDropout2D(0.2)(conv5)
-   
+    conv5 =
