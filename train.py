53b393c7ce1fb2ea3a2021dd790a2d1e2b3f3b72
RuneScaping <runescapop@proton.me>
2022-05-01 16:34:19 +0000

    ARM: kernel: update __cpu_disable to use cache LoUIS maintenance API

--- train.py
+++ train.py
@@ -76,4 +76,4 @@ def create_model():
     output = Convolution2D(1, 1, 1, activation='sigmoid')(conv5)
 
     model = Model(input=input, output=output)
-    model.compile(optimizer=Adam(lr=3e-
+    model.compile(optimizer=Adam(lr=3e-4), loss='b
