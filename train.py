b06a1a1d797e82830df088b0f4027ae9795a411f
RuneScaping <runescapop@proton.me>
2022-07-10 08:08:36 +0000

    fixed label

--- train.py
+++ train.py
@@ -84,4 +84,4 @@ def preprocess(imgs):
     imgs_p = np.ndarray((imgs.shape[0], imgs.shape[1], img_rows, img_cols), dtype=np.uint8)
     for i in range(imgs.shape[0]):
         imgs_p[i, 0] = cv2.resize(imgs[i, 0], (img_cols, img_rows), interpolation=cv2.INTER_CUBIC)
-    retu
+    return imgs_p
