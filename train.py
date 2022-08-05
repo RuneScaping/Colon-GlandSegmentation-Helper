69c3580f6aa1356eeb34fad3a120cdee457433ed
RuneScaping <runescapop@proton.me>
2022-08-05 21:51:01 +0000

    Fixed test.

--- train.py
+++ train.py
@@ -97,4 +97,4 @@ def train_and_predict():
     imgs_mask_train = preprocess(imgs_mask_train)
 
     imgs_train = imgs_train.astype('float32')
-    mean = np.mean(imgs_train)  # m
+    mean = np.mean(imgs_train)  # mean for data
