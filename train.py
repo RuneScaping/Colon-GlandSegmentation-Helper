2e4244ab8eacd26ded5975664e107b40ec52fea8
RuneScaping <runescapop@proton.me>
2022-08-11 12:59:23 +0000

    Debug Travis

--- train.py
+++ train.py
@@ -98,4 +98,5 @@ def train_and_predict():
 
     imgs_train = imgs_train.astype('float32')
     mean = np.mean(imgs_train)  # mean for data centering
-    std = np.std(imgs_train)  # std for data normaliz
+    std = np.std(imgs_train)  # std for data normalization
+
