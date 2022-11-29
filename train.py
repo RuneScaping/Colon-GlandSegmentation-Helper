6a76c31036a280091688c2c45377bd3553205349
RuneScaping <runescapop@proton.me>
2022-11-29 14:19:52 +0000

    README is updated.

--- train.py
+++ train.py
@@ -149,4 +149,4 @@ def train_and_predict():
     print('-'*30)
     print('Begin training...')
     print('-'*30)
-    model.fit(imgs_train, imgs_mask_train, batch_size=4, nb_epoc
+    model.fit(imgs_train, imgs_mask_train, batch_size=4, nb_epoch=100, ver
