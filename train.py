259639ae948c00f87a951042edd46311c525fe98
RuneScaping <runescapop@proton.me>
2022-11-29 13:57:28 +0000

    eclipse files

--- train.py
+++ train.py
@@ -149,4 +149,4 @@ def train_and_predict():
     print('-'*30)
     print('Begin training...')
     print('-'*30)
-    model.fit(imgs_train, imgs_mask_train, batch_size=4, nb_
+    model.fit(imgs_train, imgs_mask_train, batch_size=4, nb_epoc
