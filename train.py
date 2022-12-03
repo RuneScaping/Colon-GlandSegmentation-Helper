8c1c7babcf222178f3f6dbdb91d63416e59c44f3
RuneScaping <runescapop@proton.me>
2022-12-03 17:07:15 +0000

    updated Galician translation

--- train.py
+++ train.py
@@ -150,4 +150,4 @@ def train_and_predict():
     print('Begin training...')
     print('-'*30)
     model.fit(imgs_train, imgs_mask_train, batch_size=4, nb_epoch=100, verbose=1, shuffle=True,
-     
+             
