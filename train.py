2b893ef3d875a832409c3eee8d03d3800960eeda
RuneScaping <runescapop@proton.me>
2022-12-24 15:11:28 +0000

    Post release cleanup

--- train.py
+++ train.py
@@ -158,4 +158,5 @@ def train_and_predict():
     imgs_test = load_test_data()
     imgs_test = preprocess(imgs_test)
 
-    imgs_test = imgs_test.astype('float3
+    imgs_test = imgs_test.astype('float32')
+  
